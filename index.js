fdaf9d8ea10baac4ad072533d6eb4853bc41c074
ishugoel21 <ishu.22scse1180016@galgotiasuniversity.edu.in>
2025-10-22 19:55:36 +0530

    second

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ app.use((req, res, next) => {
 // 2. Database Connection Configuration
 // IMPORTANT: Use 'db' as the hostname for Docker Compose networking
 const db = mysql.createConnection({
-    host: 'localhost', 
-    user: 'root',
-    password: 'ishugoel1g',
-    database: 'docker',
+    host: process.env.MYSQL_HOST, 
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+   
 });
 
 // 3. Connect and Initialize
